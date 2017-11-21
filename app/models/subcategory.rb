92e876ebc5c70355eee11293661296f63cf507ad
Charles de Gheldere <charlesdegh@gmail.com>
2017-11-21 13:25:38 +0100

    updated treatments with references

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,7 +1,7 @@
 class Subcategory < ApplicationRecord
   belongs_to :category
-  # has_many :treatments
-  # has_many :specialists, through: :treatments
+  has_many :treatments
+  has_many :specialists, through: :treatments
   validates :category, presence: true
   validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :category }
 end
