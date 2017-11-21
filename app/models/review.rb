d97c8e495a7fb52c36f3215262b8dafb49fb0eda
Charles de Gheldere <charlesdegh@gmail.com>
2017-11-21 11:52:52 +0100

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,7 @@
 class Review < ApplicationRecord
-  belongs_to :user
-  belongs_to :specialist
-  has_many   :appointments
+  belongs_to   :appointments
 
   validates :title, presence: true
   validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
-  validates :client, presence: true
-  validates :specialist, presence: true
+  validates :appointment, presence: true
 end
