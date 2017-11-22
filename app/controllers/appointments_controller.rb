d82e2f36785d396bbbd23e93fb3c74f38ac7b6a6
Arjan Visser <arjan.visser@eqnon.com>
2017-11-22 17:01:34 +0100

    appointments controller added

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,44 @@
+class AppointmentsController < ApplicationController
+  def index
+    @appointments = Appointment.all
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.user_id = current_user
+    if @appointment.save
+      redirect_to appointment_path(@appointment)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update(appointment_params)
+      redirect_to appointment_path
+    else
+      render :edit
+    end
+  end
+
+  #delete not needed?
+
+  private
+
+  def appointment_params
+   params.require(:appointment).permit(:location, :status, :treatment, :start_time)
+  end
+end
