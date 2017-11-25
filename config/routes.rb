a5eb799b791fb5674768b549661a851d8e6f94dd
Charles de Gheldere <charlesdegh@gmail.com>
2017-11-25 20:47:03 +0100

    availability controller created and routes made

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root to: 'pages#home'
 
   devise_for :users, controllers: { registrations: "registrations" }
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   resource :profile, except: [:destroy] do
     resources :appointments, only: [:index]
     resources :treatments, only: [:new, :create]
+    resources :availabilities, shallow: true
   end
 
   resources :specialists, except: [:destroy] do
