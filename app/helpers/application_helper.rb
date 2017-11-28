a1ccf14d497bc53a4f6cb7a40f6ad5b1c4989c54
Charles de Gheldere <charlesdegh@gmail.com>
2017-11-28 17:51:51 +0100

    intermittent commit for testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
     def appointment_formatting(availability)
     {
       id: availability.id,
-      start: availability.start_time,
-      end: availability.end_time,
+      start: Datetime.new(availability.date.year, availability.date.month, availability.date.day, availability.start_time.hour, availability.start_time.min, availability.start_time.sec, availability.start_time.zone)
+      end: Datetime.new(availability.date.year, availability.date.month, availability.date.day, availability.end_time.hour, availability.end_time.min, availability.end_time.sec, availability.end_time.zone)
       title: availability.location,
       description: "available from: #{availability.start_time} to #{availability.end_time}",
       url: new_specialist_appointment_path(specialist_id: availability.specialist_id, availability_id: availability.id)
--- app/models/availability.rb
+++ app/models/availability.rb
@@ -6,12 +6,13 @@ class Availability < ApplicationRecord
   validates :specialist, presence: true
   validates :location, presence: true
   validates :start_time, :end_time, overlap: { scope: "specialist_id" }
+  validates :date, presence: true
 
   def calender_formatting
     {
       id: id,
-      start: start_time,
-      end: end_time,
+      start: Datetime.new(date.year, date.month, date.day, start_time.hour, start_time.min, start_time.sec, start_time.zone)
+      end: Datetime.new(date.year, date.month, date.day, end_time.hour, end_time.min, end_time.sec, end_time.zone)
       title: location,
       description: "available from: #{start_time} to #{end_time}",
       url: "/agenda/#{id}"
