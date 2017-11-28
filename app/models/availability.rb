61d95c0e403527552392919993299b2efc43b52e
Charles de Gheldere <charlesdegh@gmail.com>
2017-11-28 18:51:25 +0100

    datetime works, input is getting passed

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -11,8 +11,8 @@ class Availability < ApplicationRecord
   def calender_formatting
     {
       id: id,
-      start: Datetime.new(date.year, date.month, date.day, start_time.hour, start_time.min, start_time.sec, start_time.zone)
-      end: Datetime.new(date.year, date.month, date.day, end_time.hour, end_time.min, end_time.sec, end_time.zone)
+      start: DateTime.new(date.year, date.month, date.day, start_time.hour, start_time.min, start_time.sec, start_time.zone),
+      end: DateTime.new(date.year, date.month, date.day, end_time.hour, end_time.min, end_time.sec, end_time.zone),
       title: location,
       description: "available from: #{start_time} to #{end_time}",
       url: "/agenda/#{id}"
