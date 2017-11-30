9c38c6bda5946412466a874ae1558134c81c678f
Arjan Visser <arjan.visser@eqnon.com>
2017-11-30 15:16:09 +0100

    date picker added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,21 +16,8 @@
 
 
 $('.searchbar-input-date').datepicker({
+    multidate: true,
     todayHighlight: true,
-    beforeShowDay: function (date){
-                  if (date.getMonth() == (new Date()).getMonth())
-                    switch (date.getDate()){
-                      case 4:
-                        return {
-                          tooltip: 'Example tooltip',
-                          classes: 'active'
-                        };
-                      case 8:
-                        return false;
-                      case 12:
-                        return "green";
-                  }
-                },
-    toggleActive: true,
-    defaultViewDate: { year: 1977, month: 04, day: 25 }
+    datesDisabled: ['11/06/2017', '11/21/2017'],
+    toggleActive: true
 });
